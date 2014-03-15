1767ee57d1016dc460f67a51e2de1f961d6b46c5
Ivoline <ivolinengong@gmail.com>
2014-03-15 07:44:09 -0500

    Frequency of Word

---
+++ WordFrequencyExecute.java
@@ -0,0 +1,13 @@
+package WordFrequencyExecute;
+
+import java.io.IOException;
+
+
+public class WordFrequencyExecute {
+    public static void main(String[] args) throws IOException{
+        WordFrequency all=new WordFrequency();
+        all.openfile();       
+        
+    }
+    
+}
